3f7065542efd983cda67d97655a4c68b54a6b9b7
Ovsiannikov78 <seri_tanya@mail.ru>
2021-01-05 15:23:37 +0100

    05_01_2021 Type Script Defining Variables

---
+++ TypeScripr-training/02-variables/sample-types.ts
@@ -0,0 +1,10 @@
+let found = true;
+let grade = 90.2;
+let firstName = "John"
+let lastName = 'Gibson'
+
+console.log(found);
+console.log("The grade is " + grade);
+console.log("Hi " + firstName + " " + lastName);
+
+console.log(`Hi ${firstName} ${lastName}`);
