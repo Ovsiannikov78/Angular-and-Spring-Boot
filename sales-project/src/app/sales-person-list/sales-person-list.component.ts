cd4df40b717ef3b034d0cb5e2c2cbdf743b26519
Ovsiannikov78 <seri_tanya@mail.ru>
2021-02-04 18:54:08 +0100

    04_02_2021 Angular Crash Course - Fundamentals Sales project: Create sample data - array of SalesPerson objects

--- sales-project/src/app/sales-person-list/sales-person-list.component.ts
+++ sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {SalesPerson} from './sales-person';
 
 @Component({
   selector: 'app-sales-person-list',
@@ -6,6 +7,14 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./sales-person-list.component.css']
 })
 export class SalesPersonListComponent implements OnInit {
+
+  // create an array of objects
+  salesPersonList: SalesPerson[] = [
+    new SalesPerson('Jack', 'Gibson', 'j.gibson@gmail.com', 50000),
+    new SalesPerson('Mia', 'Jong', 'mia.jong@gmail.com', 40000),
+    new SalesPerson('John', 'Grey', 'j.grey@aol.com', 90000),
+    new SalesPerson('Ivan', 'Petrov', 'petrov@mail.de', 60000),
+  ];
 
   constructor() {
   }
