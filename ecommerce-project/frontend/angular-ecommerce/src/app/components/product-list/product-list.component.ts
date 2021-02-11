f465d78ad753fcb332cff0849687553371aa9aae
Ovsiannikov78 <seri_tanya@mail.ru>
2021-02-11 18:45:56 +0100

    11_02_201 eCommerce Project - Integrating Online Shop Template Enhance Application with Product Images

--- ecommerce-project/frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ ecommerce-project/frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,12 +1,12 @@
 import {Component, OnInit} from '@angular/core';
-import {ProductService} from '../../services/product.service';
-import {Product} from '../../common/product';
+import {ProductService} from 'src/app/services/product.service';
+import {Product} from 'src/app/common/product';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list-grid.component.html',
-  /*templateUrl: './product-list-table.component.html',*/
-  /*templateUrl: './product-list.component.html',*/
+  // templateUrl: './product-list-table.component.html',
+  // templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
@@ -27,4 +27,5 @@ export class ProductListComponent implements OnInit {
       }
     );
   }
+
 }
