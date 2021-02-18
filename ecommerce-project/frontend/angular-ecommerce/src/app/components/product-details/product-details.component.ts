bdb340ae1ae9c67f13a3422a1262620855f7e221
Ovsiannikov78 <seri_tanya@mail.ru>
2021-02-18 17:30:10 +0100

    18_02_2021 eCommerce Project - Product Master-Detail View Enhance ProductDetailsComponent to retrieve product from ProductService

--- ecommerce-project/frontend/angular-ecommerce/src/app/components/product-details/product-details.component.ts
+++ ecommerce-project/frontend/angular-ecommerce/src/app/components/product-details/product-details.component.ts
@@ -1,4 +1,7 @@
 import {Component, OnInit} from '@angular/core';
+import {Product} from '../../common/product';
+import {ProductService} from '../../services/product.service';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-product-details',
@@ -7,10 +10,27 @@ import {Component, OnInit} from '@angular/core';
 })
 export class ProductDetailsComponent implements OnInit {
 
-  constructor() {
+  product: Product;
+
+  constructor(private productService: ProductService,
+              private route: ActivatedRoute) {
   }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(() => {
+      this.handleProductDetails();
+    });
   }
 
+  private handleProductDetails() {
+
+    // get the "id" param string. convert string to a number using the "+" symbol
+    const theProductId: number = +this.route.snapshot.paramMap.get('id');
+
+    this.productService.getProduct(theProductId).subscribe(
+      data => {
+        this.product = data;
+      }
+    );
+  }
 }
