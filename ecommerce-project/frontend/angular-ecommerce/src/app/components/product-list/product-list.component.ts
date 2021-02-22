f25fe445b14f92c239120ebedad25162dea9eb80
Ovsiannikov78 <seri_tanya@mail.ru>
2021-02-22 17:14:19 +0100

    21_02_2021 eCommerce Project - Shopping Cart Status Component Add click handler for "Add to cart" button; Update ProductListComponent with click handler method

--- ecommerce-project/frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ ecommerce-project/frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -113,4 +113,9 @@ export class ProductListComponent implements OnInit {
     this.thePageNumber = 1;
     this.listProducts();
   }
+
+  addToCart(theProduct: Product) {
+
+    console.log(`Adding to cart: ${theProduct.name}, ${theProduct.unitPrice}`);
+  }
 }
