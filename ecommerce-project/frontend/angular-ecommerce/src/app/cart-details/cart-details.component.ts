267c44db4c5b71be65fa0934ff0244b70f6aa0d4
Ovsiannikov78 <seri_tanya@mail.ru>
2021-02-26 21:53:29 +0100

    26_02_2021 eCommerce Project - Shopping Cart Items CRUD - Decrement/Remove Item Quantity Add click handler for the "decrement" button on HTML template; Update CartDetailsComponent with click handler method; Modify CartDetailsComponent with supporting method

--- ecommerce-project/frontend/angular-ecommerce/src/app/cart-details/cart-details.component.ts
+++ ecommerce-project/frontend/angular-ecommerce/src/app/cart-details/cart-details.component.ts
@@ -42,4 +42,9 @@ export class CartDetailsComponent implements OnInit {
   incrementQuantity(theCartItem: CartItem) {
     this.cartService.addToCart(theCartItem);
   }
+
+  decrementQuantity(theCartItem: CartItem) {
+    this.cartService.decrementQuantity(theCartItem);
+
+  }
 }
