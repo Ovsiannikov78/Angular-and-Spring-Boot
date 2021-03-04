c95b9c4245a240c14384478c364941d8932cd4f2
Ovsiannikov78 <seri_tanya@mail.ru>
2021-03-04 08:53:12 +0100

    04_03_2021 eCommerce Project - Checkout Form - Drop-Down Lists Update checkout component to retrieve the months and years from service

--- ecommerce-project/frontend/angular-ecommerce/src/app/checkout/checkout.component.ts
+++ ecommerce-project/frontend/angular-ecommerce/src/app/checkout/checkout.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup} from '@angular/forms';
+import {Luv2ShopFormService} from '../luv2-shop-form.service';
 
 @Component({
   selector: 'app-checkout',
@@ -13,7 +14,11 @@ export class CheckoutComponent implements OnInit {
   totalPrice: number = 0;
   totalQuantity: number = 0;
 
-  constructor(private formBuilder: FormBuilder) {
+  creditCardYears: number[] = [];
+  creditCardMonths: number[] = [];
+
+  constructor(private formBuilder: FormBuilder,
+              private luv2ShopService: Luv2ShopFormService) {
   }
 
   ngOnInit(): void {
@@ -46,6 +51,23 @@ export class CheckoutComponent implements OnInit {
         expirationMonth: [''],
         expirationYear: ['']
       })
+    });
+
+    // populate credit card months
+
+    const startMonth: number = new Date().getMonth() + 1;
+    console.log('startMonth: ' + startMonth);
+
+    this.luv2ShopService.getCreditCardMonth(startMonth).subscribe(data => {
+      console.log('Retrieve credit card months: ' + JSON.stringify(data));
+      this.creditCardMonths = data;
+    });
+
+    // populate credit card years
+
+    this.luv2ShopService.getCreditCardYears().subscribe(data => {
+      console.log('Retrieve credit card years: ' + JSON.stringify(data));
+      this.creditCardYears = data;
     });
   }
 
