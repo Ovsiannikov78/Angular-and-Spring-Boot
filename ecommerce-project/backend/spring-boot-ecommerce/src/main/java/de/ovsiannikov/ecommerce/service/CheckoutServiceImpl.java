5c6d570856181158ac4f5f381ed8f2eb303e2381
Ovsiannikov78 <seri_tanya@mail.ru>
2021-03-09 07:59:57 +0100

    09_03_2021 eCommerce Project - Checkout Form - Save the Order to Database Create repository; Create service;

---
+++ ecommerce-project/backend/spring-boot-ecommerce/src/main/java/de/ovsiannikov/ecommerce/service/CheckoutServiceImpl.java
@@ -0,0 +1,59 @@
+package de.ovsiannikov.ecommerce.service;
+
+import de.ovsiannikov.ecommerce.dao.CustomerRepository;
+import de.ovsiannikov.ecommerce.dto.Purchase;
+import de.ovsiannikov.ecommerce.dto.PurchaseResponse;
+import de.ovsiannikov.ecommerce.entity.Customer;
+import de.ovsiannikov.ecommerce.entity.Order;
+import de.ovsiannikov.ecommerce.entity.OrderItem;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Set;
+import java.util.UUID;
+
+@Service
+public class CheckoutServiceImpl implements CheckoutService {
+
+    private CustomerRepository customerRepository;
+
+    public CheckoutServiceImpl(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @Override
+    @Transactional
+    public PurchaseResponse placeOrder(Purchase purchase) {
+
+        // retrieve the order info from dto
+        Order order = purchase.getOrder();
+
+        // generate tracking number
+        String orderTrackingNumber = generateOrderTrackingNumber();
+        order.setOrderTrackingNumber(orderTrackingNumber);
+
+        // populate order with orderItems
+        Set<OrderItem> orderItems = purchase.getOrderItems();
+        orderItems.forEach(item -> order.add(item));
+
+        // populate order with billingAddress and shippingAddress
+        order.setBillingAddress(purchase.getBillingAddress());
+        order.setShippingAddress(purchase.getShippingAddress());
+
+        // populate customer with order
+        Customer customer = purchase.getCustomer();
+        customer.add(order);
+
+        // save to the database
+        customerRepository.save(customer);
+
+        // return a response
+        return new PurchaseResponse(orderTrackingNumber);
+    }
+
+    private String generateOrderTrackingNumber() {
+
+        // generate a random UUID number(UUID Version-4)
+        return UUID.randomUUID().toString();
+    }
+}
